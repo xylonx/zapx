8730b491d781be232f3f894eae6251d9c0001c0c
xylonx <xylonx@dingtalk.com>
2021-09-02 21:25:13 +0800

    :art: shrink var

--- decoder/sky_decoder.go
+++ decoder/sky_decoder.go
@@ -15,14 +15,13 @@ var (
 type skywalkingDecoder struct{}
 
 func (decoder skywalkingDecoder) DecodeCtx(ctx context.Context) []zap.Field {
-	serviceName := go2sky.ServiceName(ctx)
-	serviceInstanceName := go2sky.ServiceInstanceName(ctx)
-	traceId := go2sky.TraceID(ctx)
-	traceSegId := go2sky.TraceSegmentID(ctx)
-	spanId := go2sky.SpanID(ctx)
 	return []zap.Field{
 		zap.String("SW_CTX", fmt.Sprintf("[%s,%s,%s,%s,%d]",
-			serviceName, serviceInstanceName, traceId, traceSegId, spanId,
+			go2sky.ServiceName(ctx),
+			go2sky.ServiceInstanceName(ctx),
+			go2sky.TraceID(ctx),
+			go2sky.TraceSegmentID(ctx),
+			go2sky.SpanID(ctx),
 		)),
 	}
 }
